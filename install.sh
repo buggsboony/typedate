807d9396559629d7a535a2e41d7121cebb99e6c7
buggsboony <boonydeboon@gmail.com>
2026-03-01 13:42:12 -0400

    Try to make it work on startup

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# console colors 2021-10-03 11:04:37
-
-# New Console Colors
-BYELL='\033[1;33m'
+# 2026-03-01 12:56:48 - New Console Colors
+NC='\033[0m'
 RED='\033[38;5;1m'
+BRED='\033[1;31m'
+GREEN='\033[0;32m'
 BLUE='\033[38;5;4m'
+YELL='\033[38;5;184m'
+BYELL='\033[1;33m'
+ORAN='\033[0;33m'
 GRAY='\033[38;5;8m'
 
-BRED='\033[1;31m'
+
 MAG='\033[0;35m'
 BMAG='\033[1;35m'
 VIOLET='\033[38;5;5m'
@@ -15,11 +18,10 @@ TURQUOISE='\033[38;5;37m'
 VFLUO='\033[38;5;40m'
 GOLD='\033[38;5;100m'
 LGREEN='\033[38;5;6m'
-GREEN='\033[0;32m'
+
 BGREEN='\033[1;32m'
 
-ORAN='\033[0;33m'
-NC='\033[0m'
+
 
 
 #install stuff
@@ -38,15 +40,15 @@ echo "Directly copied to /usr/bin for maximum execution speed"
 sudo cp "$PWD/$what$extension" /usr/bin/$what
 
 
-echo -e "${ORAN}Installing ydotool (supports both Wayland and X11)...?${NC}\n"
+echo -e "${YELL}Installing ydotool (supports both Wayland and X11)...?${NC}\n"
 sudo pacman -S ydotool
 
 
-echo -e "${ORAN}Deploy $service_basename.service...?${NC}\n"
+echo -e "${YELL}Deploy $service_basename.service...?${NC}\n"
 sudo cp "$service_basename.service" "/etc/systemd/system/$service_basename.service"
 
 
-echo -e "${BLUE}Enable and start ydotool socket service.."
+echo -e "${ORAN}Enable and start ydotool socket service..${NC}"
 sudo systemctl daemon-reload
 sudo systemctl enable ydotool.service
 sudo systemctl start ydotool.service
@@ -56,7 +58,7 @@ sudo systemctl start ydotool.service
 #Ajoutez cette ligne dans ~/.profile ou ~/.bashrc :
 #export YDOTOOL_SOCKET=/tmp/.ydotool_socket
 
-echo -e  "${ORAN}Installing ydotool-rebind for AZERTY layout...${NC}\n"
+echo -e  "${YELL}Installing ydotool-rebind for AZERTY layout...${NC}\n"
 
 git clone https://github.com/david-vct/ydotool-rebind.git 
 cd ydotool-rebind
